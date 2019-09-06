bfbe674964c557852f1f86f1a0705456f696371a
James Lyons <The_Stratigest@hotmail.com>
2019-09-06 16:48:29 -0400

    Did work on demo 2 module.  Code for the primary object is functional and tied to the mouse cursor.  Button that launches the demo is also hidden after clicking on it, preventing users from clicking it multiple times.

--- js/canvasDemo.js
+++ js/canvasDemo.js
@@ -1,5 +1,31 @@
+
+// Variables:
+var square
+var background
+var d2 = {
+    canvas: document.getElementById("movingCanvas"),
+    start: function () {
+        this.canvas.width = 800;
+        this.canvas.height = 600;
+        this.canvas.style.cursor = "none";
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateCanvas, 10);
+        window.addEventListener("mousemove", function (e) {
+            d2.x = e.pageX;
+            d2.y = e.pageY;
+        })
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
+    }
+}
+
 // Object Constructors:
-// Testing to see if we can perform a successful push: 
+
 
 //Basic constructor for shapes:
 function canvasObject(height, width, color, x, y) {
@@ -10,50 +36,84 @@ function canvasObject(height, width, color, x, y) {
     this.x = x;
     this.y = y;
     this.update = function () {
-        ctx = myGameArea.context;
+        ctx = d2.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
 }
-this.newPos = function () {
-    this.gravitySpeed;
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.type == "background") {
-        if (this.x == -(this.width)) {
+
+//Constructor for the background:
+function canvasBackground(imgSrc, height, width, x, y) {
+    this.img = new Image();
+    this.img.src = imgSrc;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function () {
+        ctx = d2.context;
+        ctx.drawImage(this.img, this.x + this.width, this.y, this.width, this.height)
+    }
+    // Function that redraws the object on the Canvas:
+    this.newPos = function () {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x == -(this.width + 150)) {
             this.x = 0;
         }
+
     }
+
 }
-}    
-}
 
+// Function that refreshed the Canvas:
+function updateCanvas() {
 
-// Second Demo:
+    d2.clear();
+    d2.frameNo += 1;
 
-var dII = {
-    canvas: document.getElementById("movingCanvas"),
-    start: function () {
-        this.canvas.width = 700;
-        this.canvas.height = 350;
-        this.canvas.style.cursor = "none";
-        this.context = this.canvas.getContext("2d");
-        this.frameNo = 0;
-        this.interval = setInterval(updateCanvas, 10);
-    },
-    clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    stop: function () {
-        clearInterval(this.interval);
+    // Update Background to make it look like it is moving:
+    background.speedX = 0;
+    background.newPos();
+    background.update();
+
+    // Updates the position of the square to the location of the
+    // cursor.
+    (d2.x && d2.y)
+    {
+        if (square.x > d2.x) {
+            square.x = d2.x;
+        }
+        else {
+            square.x = (d2.x - 250);
+        }
+        if (square.y > d2.y) {
+            square.y = d2.y;
+        }
+        else {
+            square.y = (d2.y - 250);
+        }
     }
+    square.newPos();
+    square.update();
 }
 
+// Function that initializes the demo when called.
 function startDemo() {
-    dII.start();
+    square = new canvasObject(100, 100, "orange", 400, 150);
+    background = new canvasBackground("media/canvasBG.jpg", 600, 800, 800, 0)
+    d2.start();
 }
 
-
-// Button event listeners: 
-
-document.getElementById("d2Btn").addEventListener("click", function () { });
+// Button event listeners:
+document.getElementById("d2Btn").addEventListener("click", function () {
+    document.getElementById("d2Btn").style.display = "none";
+    startDemo();
+});
